5d3f364135ca0b1ddc5e3a00f9960a81fae16891
Ivana Ruzickova <shopping@mezitrny.cz>
2020-11-13 19:20:22 +0100

    JS komponenty pro O Pavlíně/Ivaně, GH+LI links, img do složky

---
+++ o-ivane.js
@@ -0,0 +1,97 @@
+'use strict';
+
+/* ---------- DATA PRO SEKCI O IVANĚ ---------- */
+
+const skillsIvana = [
+  {
+    question: 'Kurzy',
+    answer:
+      'Czechitas kurzy Tvořím Web od A do Z 1, Tvořím Web od A do Z 2, Digitální akademie Web',
+  },
+  {
+    question: 'Dovednosti',
+    answer:
+      'HTML, CSS, Sass, Bootstrap, BEM, JavaScript, React; Miro, Figma, Trello',
+  },
+  {
+    question: 'Pracovní zkušenosti',
+    answer: 'koordinátor tréninků bezpečnosti na cestách',
+  },
+];
+
+const quizIvana = [
+  { question: 'Nejoblíbenější aktivita', answer: 'tanec' },
+  { question: 'Nejoblíbenější seriál', answer: 'Castle na zabití' },
+  { question: 'Nejoblíbenější kniha', answer: 'Saturnin od Zdeňka Jirotky' },
+  { question: 'Nejoblíbenější místo', answer: 'Dijon' },
+  { question: 'Nejoblíbenější jídlo', answer: 'kuře na paprice' },
+];
+
+/* ---------- KOMPONENTY PRO SEKCI O IVANĚ ---------- */
+
+const AboutUsListItem = (props) => {
+  const aboutListItem = document.createElement('li');
+  aboutListItem.className = 'about__list-item';
+  aboutListItem.innerHTML = `
+    <h4 class="about__list-heading">${props.question}</h4>
+    <p>${props.answer}</p>
+  `;
+
+  return aboutListItem;
+};
+
+const AboutUsList = (props) => {
+  const aboutList = document.createElement('ul');
+  aboutList.className = 'about__list';
+
+  props.forEach((prop) => {
+    aboutList.appendChild(AboutUsListItem(prop));
+  });
+
+  return aboutList;
+};
+
+const AboutUsSectionSkills = () => {
+  const aboutSection = document.createElement('section');
+  aboutSection.className = `about about--upper`;
+
+  aboutSection.innerHTML =
+    `
+    <img class="photo" src="#" alt="Ivana" />
+    <article class="about__text about__text--upper">
+      <h3 class="text__heading">O mně</h3>
+  ` +
+    AboutUsList(skillsIvana).outerHTML +
+    `
+      <div class="logo__container">
+        <a href="https://www.linkedin.com/in/ivanaruzickova/">
+          <img class="logo" src="img/logoLI.png" alt="Linked-in logo" />
+        </a>
+        <a href="https://github.com/RuzeMeziTrny">
+          <img class="logo" src="img/logoGH.png" alt="GitHub logo" />
+        </a>
+      </div>
+    </article>
+  `;
+
+  return aboutSection;
+};
+
+const AboutUsSectionQuiz = () => {
+  const aboutSection = document.createElement('section');
+  aboutSection.className = `about about--lower`;
+
+  aboutSection.innerHTML =
+    `
+    <img class="photo" src="#" alt="Ivana" />
+    <article class="about__text about__text--lower">
+      <h3 class="text__heading">A co dál? Malý kvíz</h3>
+  ` +
+    AboutUsList(quizIvana).outerHTML +
+    `</article>`;
+
+  return aboutSection;
+};
+
+document.querySelector('#app').appendChild(AboutUsSectionSkills());
+document.querySelector('#app').appendChild(AboutUsSectionQuiz());
---
+++ o-pavline.js
@@ -0,0 +1,95 @@
+'use strict';
+
+/* ---------- DATA PRO SEKCI O PAVLÍNĚ ---------- */
+
+const skillsPavlina = [
+  {
+    question: 'Kurzy',
+    answer: 'Tvořím Web 1 - Czechitas, Tvořím Web 2 - Czechitas, Digitální akademie Web',
+  },
+  {
+    question: 'Dovednosti',
+    answer: 'HTML, CSS, Sass, BEM, JavaScript, React, Figma',
+  },
+  {
+    question: 'Pracovní zkušenosti',
+    answer: 'store manager, senior bank assistent',
+  },
+];
+
+const quizPavlina = [
+  { question: 'Nejoblíbenější aktivita', answer: 'Být s rodinou a přáteli. Jíst - hlavně čokoládu, spát a koukat na Simpsonovi.' },
+  { question: 'Nejoblíbenější seriál', answer: 'Simpsonovi' },
+  { question: 'Nejoblíbenější kniha', answer: '1984 - George Orwell' },
+  { question: 'Nejoblíbenější místo', answer: 'Domov - město Praha' },
+  { question: 'Nejoblíbenější jídlo', answer: '' },
+];
+
+/* ---------- KOMPONENTY PRO SEKCI O PAVLÍNĚ ---------- */
+
+const AboutUsListItem = (props) => {
+  const aboutListItem = document.createElement('li');
+  aboutListItem.className = 'about__list-item';
+  aboutListItem.innerHTML = `
+    <h4 class="about__list-heading">${props.question}</h4>
+    <p>${props.answer}</p>
+  `;
+
+  return aboutListItem;
+};
+
+const AboutUsList = (props) => {
+  const aboutList = document.createElement('ul');
+  aboutList.className = 'about__list';
+
+  props.forEach((prop) => {
+    aboutList.appendChild(AboutUsListItem(prop));
+  });
+
+  return aboutList;
+};
+
+const AboutUsSectionSkills = () => {
+  const aboutSection = document.createElement('section');
+  aboutSection.className = `about about--upper`;
+
+  aboutSection.innerHTML =
+    `
+    <img class="photo" src="img/foto_Pavlina1.jpg" alt="Pavlína" />
+    <article class="about__text about__text--upper">
+      <h3 class="text__heading">O mně</h3>
+  ` +
+    AboutUsList(skillsPavlina).outerHTML +
+    `
+      <div class="logo__container">
+        <a href="https://https://www.linkedin.com/in/pavl%C3%ADna-baldov%C3%A1-79869217a/">
+          <img class="logo" src="img/logoLI.png" alt="Linked-in logo" />
+        </a>
+        <a href="https://github.com/PavlinaBaldova">
+          <img class="logo" src="img/logoGH.png" alt="GitHub logo" />
+        </a>
+      </div>
+    </article>
+  `;
+
+  return aboutSection;
+};
+
+const AboutUsSectionQuiz = () => {
+  const aboutSection = document.createElement('section');
+  aboutSection.className = `about about--lower`;
+
+  aboutSection.innerHTML =
+    `
+    <img class="photo" src="img/foto_Pavlina2.jpg" alt="Pavlína se synem" />
+    <article class="about__text about__text--lower">
+      <h3 class="text__heading">A co dál? Malý kvíz</h3>
+  ` +
+    AboutUsList(quizPavlina).outerHTML +
+    `</article>`;
+
+  return aboutSection;
+};
+
+document.querySelector('#app').appendChild(AboutUsSectionSkills());
+document.querySelector('#app').appendChild(AboutUsSectionQuiz());
